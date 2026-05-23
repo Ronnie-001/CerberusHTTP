fed56027f059384828d6cc6d42a3d5f05b2b4fd5
Ronald <ronald.adesida@gmail.com>
2026-05-23 21:18:26 +0100

    add: constructor to router.h file.

--- include/router.h
+++ include/router.h
@@ -1,4 +1,17 @@
 #ifndef ROUTER_H
 #define ROUTER_H
 
+#include "request.h"
+class Router 
+{
+    private:
+
+        cerberus::Request _request;
+
+    public:
+
+        Router(const cerberus::Request& request) : _request(request)
+        {}
+};
+
 #endif // !ROUTER_H
