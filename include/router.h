013f10d81c4b00b0270627acb0da1f4a8f2aed18
Ronald <ronald.adesida@gmail.com>
2026-05-28 10:21:35 +0100

    chore: remove redundant comments.

--- include/router.h
+++ include/router.h
@@ -12,29 +12,12 @@ class Router
         Router(const cerberus::Request& request) : _request(request)
         {}
         
-        /**
-         * @brief: Used for checking method of the HTTP request.
-         * one of GET, POST, PUT, DELETE.
-         *
-         */
         void checkStatusCode();
             
-        /**
-         * @brief: Idenitfies if the resource requested from the client exists 
-         * on the server.
-         * @returns true or false if the resources exists.
-         */
         bool identifyResource();
     
-        /**
-         * @brief: Retrives the resource off the server.
-         * @returns the requested webpage as a raw string.
-         */
         std::string getResource();
         
-        /**
-         * @brief: appropriatley routes the given request to the correct resouce.
-         */
         void routeRequest();
 };
 
