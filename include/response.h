1afedcc3205a251c5d48add21415cfabd6b4086e
Ronald <ronald.adesida@gmail.com>
2026-07-21 21:18:11 +0100

    add (wip): Response class design.

--- include/response.h
+++ include/response.h
@@ -1,10 +1,25 @@
 #ifndef RESPONSE_H 
 #define RESPONSE_H
 
+#include <string>
+
 namespace cerberus 
 {
 class Response 
-{};
+{
+public:
+
+    void setStatus();
+    void setHeader();
+    void setBody();
+    void setContentType();
+    
+    void getDate();
+    int getContentLength(); 
+    
+private:
+    std::string _body;
+};
 }
 
 #endif // !RESPONSE_H
